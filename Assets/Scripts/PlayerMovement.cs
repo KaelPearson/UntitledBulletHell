ff41645955bc15f05d25bedb3d302a9f0bf50f78
KaelPearson <kaelpearson@gmail.com>
2020-03-26 01:38:00 -0700

    Movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    Rigidbody2D rb;
+    public GameObject player;
+    Vector2 Movement;
+    float movementSpeed = 1;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w")) {
+            pos.y += movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s")) {
+            pos.y -= movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d")) {
+            pos.x += movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a")) {
+            pos.x -= movementSpeed * Time.deltaTime;
+        }
+        rb.MovePosition(pos);
+    }
+}
