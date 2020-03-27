ba3152b3560a259d4ed22169adbbda2d10bd5aed
KaelPearson <kaelpearson@gmail.com>
2020-03-27 04:38:37 -0700

    Added player health and death

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collision){
+        if(collision.tag == "EnemyBullet"){
+            health--;
+            if(health <= 0){
+                Destroy(gameObject);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
