b17380f1fb9dbe102dc3821bf465219482e8155d
KaelPearson <kaelpearson@gmail.com>
2020-03-27 02:37:58 -0700

    Hide gui

---
+++ Assets/Scripts/HideGUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class HideGUI : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        NetworkManagerHUD hud = FindObjectOfType<NetworkManagerHUD>();
+        if( hud != null ){
+            hud.showGUI = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
