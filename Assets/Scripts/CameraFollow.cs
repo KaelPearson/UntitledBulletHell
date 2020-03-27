b4d1dae586a57fbcf72379e74f84757dacb7255b
KaelPearson <kaelpearson@gmail.com>
2020-03-26 17:13:39 -0700

    Added shooting and camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class CameraFollow : NetworkBehaviour
+{
+
+    public Camera cam; // Drag camera into here
+ 
+    void Start()
+    {
+        // IF I'M THE PLAYER, STOP HERE (DON'T TURN MY OWN CAMERA OFF)
+        if (isLocalPlayer) return;
+ 
+        // DISABLE CAMERA AND CONTROLS HERE (BECAUSE THEY ARE NOT ME)
+        cam.enabled = false;
+        //GetComponent<PlayerControls>().enabled = false;
+        //GetComponent<PlayerMovement>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class PlayerShoot : NetworkBehaviour
+{
+    public GameObject bullet;
+    public float speed = 2;
+
+    float timer = 0;
+    public float delayBetweenBullets = 1;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(!base.isLocalPlayer){
+            return;
+        }
+
+        if (Input.GetMouseButton(0) && timer >= delayBetweenBullets) {
+            // Gets MousePos and sets the Z to 0 because 2d field
+            Vector3 shootDirection = Input.mousePosition;
+            shootDirection.z = 0.0f;
+
+            // Transforms from screen space to world space then direction is adjusted due to player position
+            shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
+            shootDirection = shootDirection-transform.position;
+
+            // Create bullet on player
+            GameObject bulletInstance = Instantiate(bullet, transform.position, Quaternion.Euler(new Vector3(0,0,0)));
+
+            // Position of bullet becomes normalized to make same speed everywhere
+            Vector2 pos = new Vector2(shootDirection.x, shootDirection.y).normalized;
+
+            // Adds force as a impulse
+            bulletInstance.GetComponent<Rigidbody2D>().AddForce(pos * speed, ForceMode2D.Impulse);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
